47d2dd4f661f87a92ba3091b362b8dc2dd0d0aa5
Yarik-333 <gorokhov.yaroslav03@gmail.com>
2023-03-26 07:48:10 +0300

    исправляет ошибки

--- js/messages.js
+++ js/messages.js
@@ -1,20 +1,62 @@
-// Получаем ссылки на элементы шаблонов
-const successTemplate = document.querySelector('.success');
-const errorTemplate = document.querySelector('.error');
+import {isEscapeKey} from './utils.js';
+import {body} from './form.js';
 
-function createElementFromTemplate(template) {
-  const element = template.content.cloneNode(true).firstElementChild;
-  return element;
-}
+const successTemplate = document.querySelector('#success').content.querySelector('.success');
+const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+const successFragment = document.createDocumentFragment();
+const errorFragment = document.createDocumentFragment();
 
-function showSuccessMessage() {
-  const successElement = createElementFromTemplate(successTemplate);
-  document.body.appendChild(successElement);
-}
 
-function showErrorMessage() {
-  const errorElement = createElementFromTemplate(errorTemplate);
-  document.body.appendChild(errorElement);
-}
+const showSuccessMessage = () => {
+  const successMessage = successTemplate.cloneNode(true);
+  successFragment.appendChild(successMessage);
+  body.appendChild(successFragment);
+  const buttonSuccess = document.querySelector('.success__button');
+  const sectionSuccess = document.querySelector('.success');
+  const successInner = document.querySelector('.success__inner');
+  document.addEventListener('keydown', (evt) => {
+    if (isEscapeKey(evt)) {
+      sectionSuccess.remove();
+    }
+  });
+
+  sectionSuccess.addEventListener(('click'), (evt) => {
+    if (evt.target === buttonSuccess) {
+      sectionSuccess.remove();
+    }
+  });
+
+  sectionSuccess.addEventListener(('click'), (evt) => {
+    if (evt.target === successInner) {
+      sectionSuccess.remove();
+    }
+  });
+};
+
+const showErrorMessage = () => {
+  const errorMessage = errorTemplate.cloneNode(true);
+  errorFragment.appendChild(errorMessage);
+  body.appendChild(errorFragment);
+  const buttonError = document.querySelector('.error__button');
+  const sectionError = document.querySelector('.error');
+  const errorInner = document.querySelector('.error__inner');
+  document.addEventListener('keydown', (evt) => {
+    if (isEscapeKey(evt)) {
+      sectionError.remove();
+    }
+  });
+
+  sectionError.addEventListener(('click'), (evt) => {
+    if (evt.target === buttonError) {
+      sectionError.remove();
+    }
+  });
+
+  sectionError.addEventListener(('click'), (evt) => {
+    if (evt.target === errorInner) {
+      sectionError.remove();
+    }
+  });
+};
 
 export{ showErrorMessage, showSuccessMessage };
